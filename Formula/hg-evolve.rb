e2e0e05213b8050e1c88b18dd8feff605671306a
Łukasz Krawiec <lmkrawiec@gmail.com>
2023-11-24 23:49:51 +0100

    Use python 3.12 for hg-git and hg-evolve

--- Formula/hg-evolve.rb
+++ Formula/hg-evolve.rb
@@ -5,6 +5,8 @@ class HgEvolve < Formula
   homepage "https://www.mercurial-scm.org/doc/evolution/user-guide.html"
   license "GPL-2.0-or-later"
 
+  revision 1
+
   stable do
     url "https://files.pythonhosted.org/packages/b2/ec/986120fab219b21158aeed208a7e60122a59b12ef8151d2c3d44520b64a1/hg-evolve-11.1.0.tar.gz"
     sha256 "b0cbc7bc7c0bb8c4f42da1f65c50de3ee78f6f00978ef97ae9019d11147493a8"
@@ -15,21 +17,17 @@ class HgEvolve < Formula
     strategy :pypi
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, monterey: "a0c9ec9732f158772fa980ca6527682b56769324decbf9f22ea13d4e5e1dbb44"
-  end
-
   head do
     # Nothing to do
   end
 
   depends_on "mercurial"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
 
   def install
     virtualenv_install_with_resources
 
-    site_packages = Language::Python.site_packages("python3.11")
+    site_packages = Language::Python.site_packages("python3.12")
     (prefix/site_packages/"homebrew-#{@name}.pth").write libexec/site_packages
   end
 
--- Formula/hg-git.rb
+++ Formula/hg-git.rb
@@ -5,14 +5,11 @@ class HgGit < Formula
   homepage "https://hg-git.github.io"
   license "GPL-2.0-or-later"
 
+  revision 1
+
   stable do
-    url "https://files.pythonhosted.org/packages/6f/ac/78a739bb37cf0f70db9cf115263b4e30daf5e35551d48a2a881d6b8894a4/hg-git-1.0.2.tar.gz"
-    sha256 "5a840e87a70a15c9c1e06196bc3ed5955e772f11e9b1803e88c0c9f55af03416"
-    # Fix compatibility with Mercurial > 6.4. Remove in next release.
-    patch do
-      url "https://foss.heptapod.net/mercurial/hg-git/-/commit/9a52223a95e9821b2f2b544ab5a35e06963da3f1.patch"
-      sha256 "969f2900f0b4084fca21bc2368b82f5acef8f8241aa39e52501fd30264a5f59d"
-    end
+    url "https://files.pythonhosted.org/packages/41/9a/e8d24706580160636df8380763f81852a635a438a4a2f053d59ace54d3ca/hg-git-1.0.3.tar.gz"
+    sha256 "1ee170456fd2b86af3317f5bb6d76aced151075f5d659345e58f3e7bd800416c"
   end
 
   livecheck do
@@ -25,7 +22,7 @@ class HgGit < Formula
   end
 
   depends_on "mercurial"
-  depends_on "python@3.11"
+  depends_on "python@3.12"
 
   resource "dulwich" do
     url "https://files.pythonhosted.org/packages/57/e0/1b5f95c2651284a5d4fdfb2cc5ecad57fb694084cce59d9d4acb7ac30ecf/dulwich-0.21.6.tar.gz"
@@ -40,7 +37,7 @@ class HgGit < Formula
   def install
     virtualenv_install_with_resources
 
-    site_packages = Language::Python.site_packages("python3.11")
+    site_packages = Language::Python.site_packages("python3.12")
     (prefix/site_packages/"homebrew-#{@name}.pth").write libexec/site_packages
   end
 
